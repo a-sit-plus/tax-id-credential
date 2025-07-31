7520bb53412fee97efb7834f8c47b17120dba92f
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-31 12:51:04 +0200

    Release 1.2.0

--- taxid/build.gradle.kts
+++ taxid/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                api("at.asitplus.wallet:vck:5.8.0-SNAPSHOT")
+                api("at.asitplus.wallet:vck:5.8.0")
             }
         }
     }
--- taxid/src/commonTest/kotlin/at/asitplus/wallet/taxid/KotestConfig.kt
+++ taxid/src/commonTest/kotlin/at/asitplus/wallet/taxid/KotestConfig.kt
@@ -2,13 +2,11 @@ package io.kotest.provided
 
 import at.asitplus.test.XmlReportingProjectConfig
 import at.asitplus.wallet.taxid.Initializer
-import at.asitplus.wallet.taxid.Initializer2025
 
 
 /** Wires KMP JUnit XML reporting */
 class ProjectConfig : XmlReportingProjectConfig() {
     init {
         Initializer.initWithVCK()
-        Initializer2025.initWithVCK()
     }
 }
